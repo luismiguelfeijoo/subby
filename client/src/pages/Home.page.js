8feea5ebb0f31d9b945b512aacbaaab00487c865
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-04-01 16:16:34 +0200

    Add antd and config needed

--- client/src/pages/Home.page.js
+++ client/src/pages/Home.page.js
@@ -2,11 +2,15 @@ import React, { useEffect, useContext } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { UserContext } from '../../lib/auth.api';
 import { withProtected } from '../../lib/protectedRoute';
-
+import { Button } from 'antd';
 export const HomePage = withProtected(
   withRouter(({ history }) => {
     const { user, setLoading } = useContext(UserContext);
-    return <div>Hola</div>;
+    return (
+      <div>
+        <Button>Hola</Button>
+      </div>
+    );
   }),
   {
     redirect: true,
