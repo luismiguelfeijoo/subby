9086ada4d5a4f8e597829728a89833d40ae1b00a
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-04-06 17:54:00 +0200

    Add withprotected to the company router

--- client/src/routes/Company.router.js
+++ client/src/routes/Company.router.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { withTypeUser } from '../../lib/protectedTypeUser';
+import { withProtected } from '../../lib/protectedRoute';
 
-export const NewPlanRouter = withTypeUser(
-  ({ match }) => {
-    return (
-      <Switch>
-        <Route path={`${match.url}/subscriptions`} exact component={} />
-      </Switch>
-    );
-  },
-  { redirectTo: '/', type: 'coordinator' }
+export const NewPlanRouter = withProtected(
+  withTypeUser(
+    ({ match }) => {
+      return (
+        <Switch>
+          <Route path={`${match.url}/subscriptions`} exact component={} />
+        </Switch>
+      );
+    },
+    { redirectTo: '/', type: 'coordinator' }
+  )
 );
