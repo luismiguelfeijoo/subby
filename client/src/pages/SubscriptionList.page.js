eebda8b50b708604c545ff400fd5f8e27053726c
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-04-09 17:35:34 +0200

    Add search bar on list

--- client/src/pages/SubscriptionList.page.js
+++ client/src/pages/SubscriptionList.page.js
@@ -5,7 +5,7 @@ import { withProtected } from '../../lib/protectedRoute';
 import { LayoutTemplate } from '../components/Layout';
 import { withTypeUser } from '../../lib/protectedTypeUser';
 import { getSubscriptions } from '../../lib/auth.api';
-import { List, Divider } from 'antd';
+import { List, Divider, Input } from 'antd';
 import { DatePicker } from 'antd';
 import moment from 'moment';
 const count = 5;
@@ -15,6 +15,7 @@ export const SubscriptionListPage = withProtected(
     withRouter(({ history }) => {
       const { user, loading, setLoading } = useContext(UserContext);
       const [data, setData] = useState([]);
+      const [filter, setFilter] = useState('');
 
       useEffect(() => {
         if (!loading) {
@@ -30,48 +31,58 @@ export const SubscriptionListPage = withProtected(
 
       return (
         <LayoutTemplate sider={true}>
+          <Input.Search
+            placeholder='Search by name'
+            onChange={event => setFilter(event.target.value)}
+            style={{ width: '30%', margin: '10px' }}
+          />
+
           <List
             itemLayout='horizontal'
             dataSource={data}
-            renderItem={sub => (
-              <List.Item
-                actions={[
-                  <Link
-                    key='list-loadmore-edit'
-                    to={`/company/subscriptions/edit/${sub._id}`}
-                  >
-                    edit
-                  </Link>,
-                  <Link
-                    key='list-loadmore-more'
-                    onClick={e => console.log(sub._id)}
-                    to='#'
-                  >
-                    delete
-                  </Link>
-                ]}
-              >
-                <List.Item.Meta
-                  title={
-                    <Link to={`/company/subscriptions/${sub._id}`}>
-                      {`${sub.name.first} ${sub.name.last}`}
+            renderItem={sub =>
+              sub.name.first.toLowerCase().includes(filter.toLowerCase()) ? (
+                <List.Item
+                  actions={[
+                    <Link
+                      key='list-loadmore-edit'
+                      to={`/company/subscriptions/edit/${sub._id}`}
+                    >
+                      edit
+                    </Link>,
+                    <Link
+                      key='list-loadmore-more'
+                      onClick={e => console.log(sub._id)}
+                      to='#'
+                    >
+                      delete
                     </Link>
-                  }
-                  description={
-                    <>
-                      <div>
-                        {sub.parents.reduce((acc, parent) => {
-                          return acc !== ''
-                            ? `${acc}, ${parent.name.first} ${parent.name.last}`
-                            : `Clients: ${parent.name.first} ${parent.name.last}`;
-                        }, '')}
-                      </div>
-                      {sub.level && <div>Level: {sub.level}</div>}
-                    </>
-                  }
-                />
-              </List.Item>
-            )}
+                  ]}
+                >
+                  <List.Item.Meta
+                    title={
+                      <Link to={`/company/subscriptions/${sub._id}`}>
+                        {`${sub.name.first} ${sub.name.last}`}
+                      </Link>
+                    }
+                    description={
+                      <>
+                        <div>
+                          {sub.parents.reduce((acc, parent) => {
+                            return acc !== ''
+                              ? `${acc}, ${parent.name.first} ${parent.name.last}`
+                              : `Clients: ${parent.name.first} ${parent.name.last}`;
+                          }, '')}
+                        </div>
+                        {sub.level && <div>Level: {sub.level}</div>}
+                      </>
+                    }
+                  />
+                </List.Item>
+              ) : (
+                <></>
+              )
+            }
           />
         </LayoutTemplate>
       );
