e84adca4164578f271f71bfd0429aa3bfa2de961
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-04-12 22:43:39 +0200

    Update layout

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,6 +1,13 @@
 import React, { useState, useContext } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-
+import {
+  DesktopOutlined,
+  LogoutOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  ProfileOutlined
+} from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
@@ -13,11 +20,16 @@ import { doLogout, UserContext } from '../../../lib/auth.api';
 
 export const LayoutTemplate = ({ children, sider = false, currentPage }) => {
   const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = collapsed => {
+    setCollapsed(collapsed);
+  };
   return (
     <Layout style={{ minHeight: '100vh' }}>
       {sider ? (
         <>
-          <Sider width={200} collapsible collapsed={collapsed}>
+          <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+            <div style={{ height: '50px' }}></div>
             <SiderMenu selection={currentPage} history={history} />
           </Sider>
           <Layout style={{ minHeight: '100vh' }}>
@@ -54,9 +66,20 @@ const SiderMenu = withRouter(({ selection, history }) => {
 
   return (
     <Menu mode='inline' defaultSelectedKeys={[selection]}>
-      <SubMenu key='User' title='User'>
-        <Menu.Item key='Profile' onSelect={() => <Redirect to='profile' />}>
-          Profile
+      <SubMenu
+        key='User'
+        title={
+          <>
+            <UserOutlined />
+            <span className='nav-text'>User</span>
+          </>
+        }
+      >
+        <Menu.Item key='Profile'>
+          <span className='nav-text'>
+            <ProfileOutlined />
+            <span>Profile</span>
+          </span>
         </Menu.Item>
         <Menu.Item
           key='Logout'
@@ -66,17 +89,36 @@ const SiderMenu = withRouter(({ selection, history }) => {
             history.push('/');
           }}
         >
-          Log Out
+          <LogoutOutlined />
+          <span>Logout</span>
         </Menu.Item>
       </SubMenu>
-      <SubMenu key='/company' title='Company'>
+
+      <SubMenu
+        key='/company'
+        title={
+          <>
+            <DesktopOutlined />
+            <span className='nav-text'>Company</span>
+          </>
+        }
+      >
         <Menu.Item key='subscriptionsList'>
-          <Link to='/company/subscriptions'>Subscriptions</Link>
+          <Link to='/company/subscriptions' className='nav-text'>
+            Subscriptions
+          </Link>
         </Menu.Item>
         <Menu.Item key='clientsList'>
-          <Link to='/company/clients'>Clients</Link>
+          <Link to='/company/clients' className='nav-text'>
+            Clients
+          </Link>
         </Menu.Item>
       </SubMenu>
+      <Menu.Item key='Chat'>
+        <span className='nav-text'>
+          <span>Chat</span>
+        </span>
+      </Menu.Item>
     </Menu>
   );
 });
