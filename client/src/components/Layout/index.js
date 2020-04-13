078b00f00f2f8cc6236e9ab44118b515e10f5f36
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-04-13 12:39:49 +0200

    Update styles of the layout

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -3,10 +3,13 @@ import { Link, withRouter } from 'react-router-dom';
 import {
   DesktopOutlined,
   LogoutOutlined,
+  FileSearchOutlined,
   FileOutlined,
+  AuditOutlined,
   TeamOutlined,
   UserOutlined,
-  ProfileOutlined
+  ProfileOutlined,
+  PlusOutlined
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 const { SubMenu } = Menu;
@@ -20,7 +23,7 @@ import { doLogout, UserContext } from '../../../lib/auth.api';
 
 export const LayoutTemplate = ({ children, sider = false, currentPage }) => {
   const [collapsed, setCollapsed] = useState(false);
-
+  const [broken, setBroken] = useState(false);
   const onCollapse = collapsed => {
     setCollapsed(collapsed);
   };
@@ -29,7 +32,15 @@ export const LayoutTemplate = ({ children, sider = false, currentPage }) => {
     <Layout style={{ minHeight: '100vh' }}>
       {sider ? (
         <>
-          <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={onCollapse}
+            breakpoint='lg'
+            onBreakpoint={broken => {
+              setBroken(broken);
+            }}
+          >
             <div style={{ height: '50px' }}></div>
             <SiderMenu selection={currentPage} history={history} />
           </Sider>
@@ -46,7 +57,9 @@ export const LayoutTemplate = ({ children, sider = false, currentPage }) => {
         </>
       ) : (
         <>
-          <Header>Header</Header>
+          <Header>
+            <div style={{ width: '100px' }}></div>
+          </Header>
           <Content
             style={{
               padding: 24,
@@ -72,7 +85,13 @@ const SiderMenu = withRouter(({ selection, history }) => {
         title={
           <>
             <UserOutlined />
-            <span className='nav-text'>User</span>
+            <span className='nav-text'>
+              {user.type
+                ? user.type === 'admin'
+                  ? 'Admin'
+                  : 'Coordinator'
+                : 'Client'}
+            </span>
           </>
         }
       >
@@ -94,7 +113,6 @@ const SiderMenu = withRouter(({ selection, history }) => {
           <span>Logout</span>
         </Menu.Item>
       </SubMenu>
-
       <SubMenu
         key='/company'
         title={
@@ -104,22 +122,36 @@ const SiderMenu = withRouter(({ selection, history }) => {
           </>
         }
       >
-        <Menu.Item key='subscriptionsList'>
-          <Link to='/company/subscriptions' className='nav-text'>
-            Subscriptions
+        <Menu.Item key='new-user'>
+          <Link to='/new-user' className='nav-text'>
+            <PlusOutlined />
+            <span className='nav-text'>Add User or Sub</span>
           </Link>
         </Menu.Item>
-        <Menu.Item key='clientsList'>
-          <Link to='/company/clients' className='nav-text'>
-            Clients
+        <Menu.Item key='new-plan'>
+          <Link to='/new-plan' className='nav-text'>
+            <PlusOutlined />
+            <span className='nav-text'>Add Plan or Extra</span>
           </Link>
         </Menu.Item>
       </SubMenu>
+      <Menu.Item key='subscriptionsList'>
+        <Link to='/company/subscriptions' className='nav-text'>
+          <FileSearchOutlined />
+          <span className='nav-text'>Subscriptions</span>
+        </Link>
+      </Menu.Item>
+      <Menu.Item key='clientsList'>
+        <Link to='/company/clients' className='nav-text'>
+          <AuditOutlined />
+          <span className='nav-text'>Clients</span>
+        </Link>
+      </Menu.Item>
       <Menu.Item key='Chat'>
         <span className='nav-text'>
           <span>Chat</span>
         </span>
-      </Menu.Item>
+      </Menu.Item>{' '}
     </Menu>
   );
 });
